2a48877ba89d3d2eade744a3cffb03fd1b41af75
alexskc <alexskc@autistici.org>
2016-04-28 16:21:17 -0500

    Add status

--- project/settings.py
+++ project/settings.py
@@ -114,8 +114,8 @@ else:
     # stock django, local development.
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'newtodo-dev',
         }
     }
 
@@ -123,9 +123,9 @@ else:
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Sao_Paulo'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
