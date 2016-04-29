f61fd8bcc650d9a29f0f8f3c70026279546c3dac
alexskc <alexskc@autistici.org>
2016-04-28 19:22:06 -0500

    Added personal info to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='Django On OpenShift with Django 1.8',
+setup(name='Newtodo',
     version='0.1',
-    description='django on OpenShift',
-    author='Gregory Pacheco',
-    author_email='contato@gregorypacheco.com.br',
-    url='https://github.com/Gpzim98/Django-OpenShift-1.8.git',
+    description='A Different kind of to do list.',
+    author='alexskc',
+    author_email='alexskc@autistici.org',
+    url='newtodo-alexskc.rhcloud.com',
     install_requires=['Django==1.8.2',],
 )
