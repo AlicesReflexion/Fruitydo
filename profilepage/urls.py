e2b0a9bd3e72fc11993e1236909042188b35fdf7
alexskc <alexskc@autistici.org>
2016-05-01 14:21:08 -0500

    Implement task create/complete interface

--- profilepage/urls.py
+++ profilepage/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'profilepage'
 urlpatterns = [
-        url(r'^profile', views.profile, name='profile'),
+        url(r'^profile/(?P<task_id>[0-9]+)/complete/$', views.complete, name='complete'),
+        url(r'^profile/(?P<task_title>.+)/create/$', views.create, name='create'),
+        url(r'^profile', views.profile, name='profile')
         ]
--- profilepage/views.py
+++ profilepage/views.py
@@ -1,10 +1,24 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 from django.contrib.auth.models import User
-from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from .models import Task
+from datetime import datetime
 
 def profile(request):
     return render(request, 'profilepage/index.html')
+
+def complete(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task.complete = 1
+    task.save()
+    return HttpResponseRedirect(reverse('profilepage:profile'))
+
+def create(request, task_title):
+    entered_title = request.POST['task_title']
+    task = Task(User = request.user, task_title = entered_title, pub_date = datetime.now(), recurring = 0, complete = 0, due_date = datetime.now())
+    task.save()
+    return HttpResponseRedirect(reverse('profilepage:profile'))
 
 # Create your views here.
