4bb30f22a1a0b55897d5ffcd2ba27929e435c93d
alexskc <alexskc@autistici.org>
2016-05-01 14:42:57 -0500

    Made Home redirect to profile if logged in

--- project/views.py
+++ project/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 
 def home(request):
-	return render(request, 'home.html')
+	if request.user.is_authenticated():
+		return render(request, 'profilepage/index.html')
+	else:
+		return render(request, 'home.html')
 
 def about(request):
 	return render(request, 'about.html')
