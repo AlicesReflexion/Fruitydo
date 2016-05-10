8a9503f1a1f71ae95725ebaafbf45a99b8bc0af3
alexskc <alexskc@autistici.org>
2016-05-10 11:48:29 -0500

    Finalized edit button ish

--- profilepage/urls.py
+++ profilepage/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'profilepage'
 urlpatterns = [
+        url(r'^profile/event_fetch_raw', views.event_fetch_raw, name='event_fetch_raw'),
         url(r'^profile/event_fetch', views.event_fetch, name='event_fetch'),
         url(r'^profile/(?P<task_id>[0-9]+)/complete/$', views.complete, name='complete'),
         url(r'^profile/(?P<task_title>.+)/create/$', views.create, name='create'),
--- profilepage/views.py
+++ profilepage/views.py
@@ -26,8 +26,13 @@ def event_create(request):
     entered_description = request.POST['event_description']
     entered_task = request.POST['task']
     entered_date = request.POST['pub_date']
-    event = Event(event_description = entered_description, pub_date = entered_date, Task = get_object_or_404(Task, pk=entered_task))
-    event.save()
+    returnevent = Event.objects.filter(pub_date = entered_date, Task_id = entered_task)
+    if not returnevent:
+        event = Event(event_description = entered_description, pub_date = entered_date, Task = get_object_or_404(Task, pk=entered_task))
+        event.save()
+    else:
+        returnevent[0].event_description = entered_description
+        returnevent[0].save()
     return HttpResponseRedirect(reverse('profilepage:profile'))
 
 def event_fetch(request):
@@ -40,5 +45,15 @@ def event_fetch(request):
     else:
         formatted = (formatter(returnevent[0].event_description, filter_name='markdown'))
         return HttpResponse(formatted)
+
+def event_fetch_raw(request):
+    user = request.user
+    date = request.POST['date']
+    task = request.POST['task']
+    returnevent = Event.objects.filter(pub_date = date, Task_id = task)
+    if not returnevent:
+        return HttpResponse("Nothing happened on this day!")
+    else:
+        return HttpResponse(returnevent[0].event_description)
 
 # Create your views here.
