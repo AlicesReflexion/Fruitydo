ad5157fb4721000cbcd3cf7c4c904ef2bee095f5
alexskc <alexskc@autistici.org>
2016-05-13 20:04:55 -0500

    Move out initialization js.

---
+++ static/profilepage.js
@@ -0,0 +1,16 @@
+$(function() {
+	$(".datepicker").datepicker();
+	$(".datepicker").each(function() {
+		$(this).datepicker('option', 'altField', "#desc" + this.id);
+		$(this).datepicker('option', 'dateFormat', "yy-mm-dd");
+	})
+});
+var csrftoken = Cookies.get('csrftoken');
+
+$(document).ready(function(){
+	$('.editbutton').click(function(){
+		words = this.id.split('-');
+		$("#form-" + words[1]).css({"display":"inline"});
+		$("#" + this.id).css({"display":"none"});
+	});
+});
