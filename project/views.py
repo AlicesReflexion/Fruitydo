27104169ef74bd905355c2c2ca3d1b9be2e7e8d6
alexskc <alexskc@autistici.org>
2016-05-13 20:33:23 -0500

    Replaced duplicated render with a proper redirect.

--- project/views.py
+++ project/views.py
@@ -1,10 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from profilepage import urls
+from django.core.urlresolvers import reverse
 
 def home(request):
 	if request.user.is_authenticated():
-		return render(request, 'profilepage/index.html')
+		return HttpResponseRedirect(reverse('profilepage:profile'))
 	else:
 		return render(request, 'home.html')
 
