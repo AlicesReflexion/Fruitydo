0291382f2e0f2e40d837395421acc29d397a4623
alexskc <alexskc@autistici.org>
2016-05-19 09:17:47 -0500

    Move textarea js out of template.

--- static/profilepage.js
+++ static/profilepage.js
@@ -15,9 +15,22 @@ var csrftoken = Cookies.get('csrftoken');
 
 $(document).ready(function(){
 	$('.editbutton').click(function(){
-		words = this.id.split('-');
-		$("#form-" + words[1]).css({"display":"inline"});
-		$("#event" + words[1]).css({"display":"none"});
+		var words = this.id.split('-');
+		var taskid = words[1];
+		$("#form-" + taskid).css({"display":"inline"});
+		$("#event" + taskid).css({"display":"none"});
 		$("#" + this.id).css({"display":"none"});
+
+		var pickeddate = $("#" + taskid).datepicker("getDate");
+		var datetext = $.datepicker.formatDate('yy-mm-dd', pickeddate);
+		$.post("/accounts/profile/event_fetch_raw", {
+			date: datetext,
+			task: taskid,
+			csrfmiddlewaretoken: csrftoken,
+		})
+		.done(function(rawdata){
+			$("#textarea" + taskid).val(rawdata);
+		});
+
 	});
 });
