d82e2ebbdb7b12aba89b17fd5475d0fe9b97a842
alexskc <alexskc@autistici.org>
2016-06-06 10:15:20 -0500

    Add redirect for logged in user.

--- demopage/views.py
+++ demopage/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 def demo(request):
-    return render(request, 'demo/index.html')
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('profilepage:profile'))
+    else:
+        return render(request, 'demo/index.html')
 # Create your views here.
