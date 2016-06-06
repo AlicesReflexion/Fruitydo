29ee8bf4bb7837134456fec0c23fb4c167c22299
alexskc <alexskc@autistici.org>
2016-06-06 12:44:26 -0500

    Generate calendar widgets from object.

--- static/demopage.js
+++ static/demopage.js
@@ -1,9 +1,38 @@
+const calendarhtml = `<h3>%title%</h3>
+<div>
+  <table class="task_calendar">
+    <tr>
+      <td>
+	<div class="datepicker" id="%id%"></div>
+      </td>
+      <td>
+	<div class="eventbox" id="event%id%"></div>
+	<div class="editbuttons">
+	</div>
+      </td>
+    </tr>
+  </table>
+</div>
+`;
+
 $(document).ready(function() {
   var tasks = Cookies.getJSON("tasks");
+  var taskhtml = "";
   if (typeof tasks === "undefined") {
     tasks = [];
   }
-  console.log(tasks);
+
+  tasks.forEach(function(element, index, array) {
+    taskhtml += calendarhtml;
+    taskhtml = taskhtml.replace(/%title%/g, element.title);
+    taskhtml = taskhtml.replace(/%id%/g, index);
+    $(".incompletetasks").html(taskhtml);
+  });
+
+  $(".accordion").accordion({
+    collapsible: true
+  });
+  $(".datepicker").datepicker();
 
   $(".newtaskbutton").click(function() {
     var tasktitle = $(".task_title").val();
@@ -12,6 +41,7 @@ $(document).ready(function() {
     tasks.push(task);
     console.log(tasks);
     Cookies.set('tasks', tasks);
+    location.reaload();
   });
 
   $(".duecalendar").datepicker({
@@ -22,7 +52,7 @@ $(document).ready(function() {
   $(".dueinput").focus(function() {
     $(".duecalendar").slideDown(400);
   });
- 
+
   $(".dueinput").focusout(function() {
     $(".duecalendar").slideUp(400);
   });
