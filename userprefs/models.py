6ad08e8e3964840dcf7a501720642bff47fc1606
alexskc <alexskc@autistici.org>
2016-06-14 19:56:33 -0500

    Add server-side OTP key for users.

--- userprefs/models.py
+++ userprefs/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+import pyotp
 
 class user_preferences(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     otp = models.BooleanField(default=False)
+    otpkey = models.CharField(max_length=16, default=pyotp.random_base32())
 
 # Create your models here.
