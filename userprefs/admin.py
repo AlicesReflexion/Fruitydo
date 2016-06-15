df6598560154aa71544c93c3d5ef19a7b4d47353
alexskc <alexskc@autistici.org>
2016-06-14 19:52:01 -0500

    Add OTP bool to admin interface.

--- userprefs/admin.py
+++ userprefs/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from userprefs.models import user_preferences
+
+class UserprefsInline(admin.StackedInline):
+    model = user_preferences
+    can_delete = False
+    verbose_name_plural = 'preferences'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserprefsInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 # Register your models here.
