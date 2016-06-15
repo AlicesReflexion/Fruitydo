6bf8227ee6abb4a1da6a164b587ac430c7061d6a
alexskc <alexskc@autistici.org>
2016-06-15 15:02:51 -0500

    Fix qr_code resolve.

--- userprefs/urls.py
+++ userprefs/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'userprefs'
 urlpatterns = [
+    url(r'^enable_otp/qr_code', views.otp_qrcode, name='otp_qrcode'),
     url(r'^enable_otp', views.enable_otp, name='enable_otp'),
-    url(r'^enable_otp/qr_code', views.otp_qrcode, name='otp_qrcode')
-]
+    ]
