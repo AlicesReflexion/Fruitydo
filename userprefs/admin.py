a1e4e258a8b38dbe957e76c63ffb2df06dcb9092
alexskc <alexskc@autistici.org>
2016-06-15 16:02:58 -0500

    Add enabling otp

--- userprefs/admin.py
+++ userprefs/admin.py
@@ -3,3 +3,14 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
 from userprefs.models import Userpreference
+
+class Userprefinline(admin.StackedInline):
+    model = Userpreference
+    can_delete = False
+    verbose_name_plural = "Preferences"
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (Userprefinline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- userprefs/views.py
+++ userprefs/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from .models import Userpreference
+import pyotp
+import qrcode
 
 def enable_otp(request):
     return render(request, 'userprefs/enableotp.html')
 
 def otp_qrcode(request):
-    return HttpResponse(request.user.username)
+    otpkey = pyotp.TOTP(request.user.userpreference.otpkey)
+    provision = otpkey.provisioning_uri("Fruitydo")
+    otpqr = qrcode.make(provision)
+    response = HttpResponse(content_type="image/png")
+    otpqr.save(response, "PNG")
+    return response
 
-# Create your views here.
+def confirm_otp(request):
+    user = request.user
+    confirmcode = request.POST['confirmcode']
+    otpkey = pyotp.TOTP(request.user.userpreference.otpkey)
+    if confirmcode == otpkey.now():
+        user.userpreference.otp = True
+        user.userpreference.save()
+        return HttpResponseRedirect(reverse('profilepage:profile'))
+    else:
+        return HttpResponse("Codes do not appear to match.")
