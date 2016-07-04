4c158dab057d94f052c6315e2011544350a417ae
alexskc <alexskc@autistici.org>
2016-07-04 13:03:00 -0500

    Make gulp watch for changes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,25 @@
 var gulp = require('gulp');
 var cssnano = require('gulp-cssnano');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', function() {
-  return gulp.src('./resources/style/*.scss')
+var paths = {
+  styles: './resources/style/'
+};
+
+gulp.task('stylegen', function() {
+  return gulp.src(paths.styles)
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
     .pipe(cssnano())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./static/style'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch(paths.styles, ['stylegen']);
+});
+
+gulp.task('default', ['watch', 'stylegen']);
