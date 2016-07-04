e580a431f134dcca18d1693b5a96caa0f9fd0427
alexskc <alexskc@autistici.org>
2016-07-04 12:02:57 -0500

    Move to scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var cssnano = require('gulp-cssnano');
+var sass = require('gulp-sass');
 
 gulp.task('default', function() {
-  return gulp.src('./resources/style/*.css')
+  return gulp.src('./resources/style/*.scss')
+    .pipe(sass().on('error', sass.logError))
     .pipe(cssnano())
     .pipe(gulp.dest('./static/style'));
 });
