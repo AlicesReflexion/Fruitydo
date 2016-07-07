1c1d79f9dfc91dd48af3181f620c5e1d1b1746d4
alexskc <alexskc@autistici.org>
2016-07-07 13:19:59 -0500

    Keep removing things until you find the bug.

--- gulpfile-openshift.js
+++ gulpfile-openshift.js
@@ -3,21 +3,21 @@ var NodeMod = process.env.DEPENDENCY_BASE + '/node_modules/';
 var gulp = require(NodeMod + 'gulp');
 
 // CSS plugins
-var cssnano = require(NodeMod + 'gulp-cssnano');
-var sass = require(NodeMod + 'gulp-sass');
-var autoprefixer = require(NodeMod + 'gulp-autoprefixer');
+//var cssnano = require(NodeMod + 'gulp-cssnano');
+//var sass = require(NodeMod + 'gulp-sass');
+//var autoprefixer = require(NodeMod + 'gulp-autoprefixer');
 
 // JS plugins
-var uglify = require(NodeMod + 'gulp-uglify');
+//var uglify = require(NodeMod + 'gulp-uglify');
 
-var imagemin = require(NodeMod + 'gulp-imagemin');
+//var imagemin = require(NodeMod + 'gulp-imagemin');
 
-var paths = {
-  styles: './resources/style/*.scss',
-  scriptdeps: './resources/js/**/*',
-  scripts: './resources/js/**/*.js',
-  images: './resources/branding/**/*.svg'
-};
+//var paths = {
+  //styles: './resources/style/*.scss',
+  //scriptdeps: './resources/js/**/*',
+  //scripts: './resources/js/**/*.js',
+  //images: './resources/branding/**/*.svg'
+//};
 
 /*gulp.task('copydeps', function() {
   return gulp.src(paths.scriptdeps, {base: './resources/js'})
