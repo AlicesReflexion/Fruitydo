aaacc75192502051a192d4bf392e603b2fd1e50c
Aleksander Charatonik <alexskc@autistici.org>
2016-08-15 15:49:38 -0500

    Store password in session.

--- resources/js/decrypt.js
+++ resources/js/decrypt.js
@@ -3,12 +3,18 @@ $.get("/settings/crypto_key").done(function(data){
   encryptedkey = data;
 });
 console.log(encryptedkey);
+var decryptedpass = Cookies.get('decryptedpass');
 
 $(document).ready(function() {
+  if(typeof decryptedpass != 'undefined'){
+    $(".encryptionoverlay").hide();
+  }
+
   $("#decbutton").click(function() {
     var userpass = $("#encpassword").val();
     try{
-    var decryptedpass = sjcl.decrypt(userpass, encryptedkey);
+    decryptedpass = sjcl.decrypt(userpass, encryptedkey);
+    Cookies.set('decryptedpass', decryptedpass);
     $(".encryptionoverlay").fadeOut()
     } catch(e) {
       alert("Incorrect encryption password. Try again.")
